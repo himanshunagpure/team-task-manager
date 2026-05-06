c33e70b118578c38953658da4756fd9b0373e024
Himanshu <nagpurehimanshu9@gmail.com>
2026-05-06 16:34:29 +0530

    internet

--- backend/server.js
+++ backend/server.js
@@ -89,9 +89,8 @@ const PORT =
 
 app.listen(
   PORT,
+  "0.0.0.0",
   () => {
-    console.log(
-      `Server running on port ${PORT}`
-    );
+    console.log(`Server running on port ${PORT}`);
   }
 );
